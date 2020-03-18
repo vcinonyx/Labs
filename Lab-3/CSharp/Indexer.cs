2533e91f6b0614a2fb57e2f5bb54f33702062322
vcinonyx <vcinonyx@gmail.com>
2020-03-19 00:24:51 +0300

    lab 3 done

---
+++ Lab-3/CSharp/Indexer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+
+namespace lab3
+{
+    class Indexer
+    {
+        private int n;
+        private char[,] Matrix;
+        private int intNumb;
+        private string column;
+        public Indexer()
+        {
+            n = 5;
+            Matrix = new char[n, n];
+        }
+
+
+        public void FillMatrix()
+        {
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int num = random.Next(30, 80);
+                    Matrix[i, j] = (char)(num);
+                    if (Matrix[i, j] >= 48 && Matrix[i, j] <= 57)
+                    {
+                        intNumb++;
+                    }
+                }
+            }
+        }
+
+        public void PrintMatrix()
+        {
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(Matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
+        public int IntNumb => intNumb;
+        public string this[int index]
+        {
+            get
+            {
+                column = "";
+                for (int i = 0; i < n; i++)
+                {
+                    column += Matrix[i, index];
+                }
+                return column;
+            }
+        }
+    }
+}
+
+     
---
+++ Lab-3/CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Indexer obj = new Indexer();
+            obj.FillMatrix();
+            obj.PrintMatrix();
+            Console.WriteLine(obj.IntNumb);
+            Console.WriteLine(obj[0]);
+            Console.WriteLine(obj[4]);
+        }
+    }
+}
