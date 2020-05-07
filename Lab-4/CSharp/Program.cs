a631bb2f377fd6d63302e7a519e1a859ce8c0f21
Tadzhat Marharian <53492310+vcinonyx@users.noreply.github.com>
2020-05-07 13:00:18 +0300

    lab 4 updated

--- Lab-4/CSharp/Program.cs
+++ Lab-4/CSharp/Program.cs
@@ -5,7 +5,7 @@
         static void Main()
         {
             Triangle T1 = new Triangle();
-            Triangle T2 = new Triangle(1, 2, 4, 1, 6, 8);
+            Triangle T2 = new Triangle(1, 4, 4, 1, 6, 8);
             Triangle T3 = new Triangle(T2);
             T3 = T3 * 2;
             T3 = 2 * T3;
