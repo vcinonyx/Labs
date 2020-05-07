da0675bcb8b124128430bb8b182f2ec8a85099f0
vcinonyx <vcinonyx@gmail.com>
2020-05-07 12:47:08 +0300

    added main.cpp

---
+++ Lab-6/C++/main.cpp
@@ -0,0 +1,16 @@
+#include "Expression.h"
+
+using namespace std;
+
+int main()
+{    
+    Expression example[] = { Expression(),  Expression(1, 4, 2, 45),  Expression(0, 0, 0, 41) };  
+    example[0].SetA(1);
+    example[0].SetB(2);
+    example[0].SetC(3);
+    example[0].SetD(4);
+    for (int i = 0; i < 3; i++)
+    {
+        example[i].Calculate();
+    }
+}
